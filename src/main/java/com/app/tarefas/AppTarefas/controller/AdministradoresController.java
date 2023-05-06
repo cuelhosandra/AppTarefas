b93f0a92c2bc89afb6d3c4e142865d65239358fa
Sandra Coelho <sandracoelho_br@hotmail.com>
2023-05-06 15:30:54 -0300

    ajustes visuais e rotas

---
+++ src/main/java/com/app/tarefas/AppTarefas/controller/AdministradoresController.java
@@ -0,0 +1,13 @@
+package com.app.tarefas.AppTarefas.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AdministradoresController {
+    @GetMapping("/administradores")
+    public String index(){
+        return "administradores/index";
+    }
+}
+
