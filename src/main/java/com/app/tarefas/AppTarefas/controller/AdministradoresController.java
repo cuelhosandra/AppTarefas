38f40421ac6b6be9c86a5c5300054b9398eee54c
Sandra Coelho <sandracoelho_br@hotmail.com>
2023-05-10 18:44:40 -0300

    insercao de dados e subir no front

--- src/main/java/com/app/tarefas/AppTarefas/controller/AdministradoresController.java
+++ src/main/java/com/app/tarefas/AppTarefas/controller/AdministradoresController.java
@@ -1,12 +1,24 @@
 package com.app.tarefas.AppTarefas.controller;
 
+import com.app.tarefas.AppTarefas.models.Administrador;
+import com.app.tarefas.AppTarefas.repositório.AdministradoresRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class AdministradoresController {
+
+    @Autowired
+    private AdministradoresRepo repo;
+
     @GetMapping("/administradores")
-    public String index(){
+    public String index(Model model){
+        List<Administrador> administradores = (List<Administrador>)repo.findAll();
+        model.addAttribute("administradores", administradores);
         return "administradores/index";
     }
 }
